3048e9c355c7bb7a1670aab7a3114d1bf85886ef
Lucas Passos <lucas@cmtecnologia.com.br>
2020-06-02 20:33:56 -0300

    Updates!

--- public/components/panel.js
+++ public/components/panel.js
@@ -20,7 +20,33 @@ var discordPanel = {
             method: 'GET',
             url: discordPanel.Api
         }).done(function(data) {
-            console.log(data);
+            let membersContent = "";
+
+            if (data.instant_invite) {
+                $('.widgetLogo, .widgetBtnConnect').attr('href', data.instant_invite);
+            }
+
+            $('.widgetHeaderCount').html(`<strong>${data.presence_count}</strong> Members Online`);
+
+            for (var i in data.members) {
+                let status = data.members[i].status;
+
+                status = (status.charAt(0).toUpperCase() + status.slice(1));
+
+                membersContent += `<div class="widgetMember-s">
+                                        <div class="widgetMemberAvatar">
+                                            <img alt="" src="${data.members[i].avatar_url}">
+                                            <span class="widgetMemberStatus widgetMemberStatus${status}"></span>
+                                        </div>
+                                        <span class="widgetMemberName">${data.members[i].username}</span>
+                                        <span class="widgetMemberGame">VALORANT</span>
+                                    </div>`;
+            }
+
+            if (membersContent != "") {
+                $('.widgetBody').append('<div>' + membersContent + '</div>');
+            }
+
         }).fail(function(xhr) {
             console.log(xhr.responseJSON);
         });
